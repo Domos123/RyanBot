dc378bf75e8f8a74bf6adbbe2c5dad8b26d7078f
Dominic Brady <domos@domos.me.uk>
2017-10-18 01:07:22 +0100

    Much better wiki parsing
    Parse HTML instead of WikiText
    Also we now get an image if there is one

--- commands/util/wiki.js
+++ commands/util/wiki.js
@@ -21,34 +21,49 @@ module.exports = class WikiCommand extends Command {
 
   async run(msg, args) {
     const topic = args['topic'];
-    console.log(topic);
-    wikipedia.page.data(topic,{ wikitext: true },function(response){
+    wikipedia.page.data(topic,{ content: true },function(response){
       if (response) {
-        const prettyText = parseWikiText(response.wikitext['*']);
+        const parsedPage = parseWikiHTML(response.text['*']);
         const embed = new RichEmbed()
-        .addField(response.title,prettyText)
+        .setTitle(response.title)
+        .setDescription(parsedPage.text)
         .setColor(0x00ced1)
-        .setTimestamp();
+        .setURL(encodeURI(`https:\/\/en.wikipedia.org\/wiki\/${topic}`));
+        if (parsedPage.image) {
+          embed.setThumbnail(`${parsedPage.image}`);
+        }
         msg.channel.send({embed});
       } else {
         msg.channel.send("Sorry, I couldn't find a wikipedia article about that");
       }
-    })
+    });
   }
 
 
 };
 
-function parseWikiText(s) {
-  template = new RegExp(/{{[^{]*?}}/g);
-  while (template.test(s)){
-    s = s.replace(template, ''); // remove templates
+function parseWikiHTML(s) {
+  var parsedPage = {}
+
+  //Extract image
+  var imagePattern = new RegExp(/\<img.+?src="(\S*\.(?:jpg|png|jpeg|gif|bmp))"/);
+  var matchedImg = imagePattern.exec(s);
+  if (matchedImg) {
+    parsedPage.image=`https:${matchedImg[1]}`;
   }
-  s = s.replace(/\[\[(?:[^\|\]]+\|)?/g, '') //strip the beginnings of links, including masked links
-       .replace(/\]\]/g, '') //strip the ends of links
-       .replace(/<(?:.|\n)*?>/g, '') // remove HTML tags
-       .replace(/\[[0-9]*\]|\[note [0-9]*\]/g, '') // remove citation numbers
-       .replace(/''+/g, '**') //replace emphasis formatting
-       .replace(/^[\s]*/, ''); //Strip stray whitespace at beginning
-  return s.substring(0,s.indexOf('\n'));//just return first paragraph
+
+  //Extract Text
+  var textPattern = new RegExp(/\<p\>(.+?)\<\/p\>(?!(?:\s\S)*?\/td)/);
+  var matchedText = textPattern.exec(s);
+  if (!matchedText) {
+    return null;
+  }
+
+  //Parse Text
+  parsedPage.text = matchedText[1].replace(/\<\/?i\>/g, "*")  //italics
+                                  .replace(/\<\/?b\>/g, "**") //bold
+                                  .replace(/\<\/?.+?\>/g, "") //strip HTML
+                                  .replace(/\[.+?\]/g, "");   //strip citations
+
+  return parsedPage;
 }
